078fc0fbc6d8e46064bf0911ca7f309b62291fd8
Today017 <yesterday998244353@gmail.com>
2024-06-13 06:34:09 +0900

    [Fix] Insertable Reverisble Array

--- data_structure/insrev.hpp
+++ data_structure/insrev.hpp
@@ -1,7 +1,7 @@
 #include "../../kyopro_library/template.hpp"
 
 namespace insertable_reversible_array_impl {
-	/* https://nyaannyaan.github.io/library/rbst/lazy-reversible-rbst.hpp.html */
+    /* https://nyaannyaan.github.io/library/rbst/lazy-reversible-rbst.hpp.html */
     template <typename Node>
     struct RBSTBase {
         using Ptr = Node *;
@@ -11,9 +11,7 @@ namespace insertable_reversible_array_impl {
         }
         inline void my_del(Ptr t) { delete t; }
         inline Ptr make_tree() const { return nullptr; }
-
         int size(Ptr t) const { return count(t); }
-
         Ptr merge(Ptr l, Ptr r) {
             if (!l || !r) return l ? l : r;
             if (int((rng() * (l->cnt + r->cnt)) >> 32) < l->cnt) {
@@ -26,7 +24,6 @@ namespace insertable_reversible_array_impl {
                 return update(r);
             }
         }
-
         pair<Ptr, Ptr> split(Ptr t, int k) {
             if (!t) return {nullptr, nullptr};
             push(t);
@@ -40,7 +37,6 @@ namespace insertable_reversible_array_impl {
                 return {update(t), s.second};
             }
         }
-
         Ptr build(int l, int r, const vector<decltype(Node::key)> &v) {
             if (l + 1 == r) return my_new(v[l]);
             int m = (l + r) >> 1;
@@ -49,17 +45,14 @@ namespace insertable_reversible_array_impl {
             if (m + 1 < r) pm->r = build(m + 1, r, v);
             return update(pm);
         }
-
         Ptr build(const vector<decltype(Node::key)> &v) {
             return build(0, (int)v.size(), v);
         }
-
         template <typename... Args>
         void insert(Ptr &t, int k, const Args &...args) {
             auto x = split(t, k);
             t = merge(merge(x.first, my_new(args...)), x.second);
         }
-
         void erase(Ptr &t, int k) {
             auto x = split(t, k);
             auto y = split(x.second, 1);
@@ -72,11 +65,8 @@ namespace insertable_reversible_array_impl {
             static uint64_t x_ = 88172645463325252ULL;
             return x_ ^= x_ << 7, x_ ^= x_ >> 9, x_ & 0xFFFFFFFFull;
         }
-
         inline int count(const Ptr t) const { return t ? t->cnt : 0; }
-
         virtual void push(Ptr) = 0;
-
         virtual Ptr update(Ptr) = 0;
     };
 
@@ -87,29 +77,24 @@ namespace insertable_reversible_array_impl {
         E lazy;
         int cnt;
         bool rev;
-
         LazyReversibleRBSTNode(const T &t = T(), const E &e = E())
             : l(), r(), key(t), sum(t), lazy(e), cnt(1), rev(false) {}
     };
 
-    template <typename T, typename E, T (*f)(T, T), T (*g)(T, E), E (*h)(E, E),
-              T (*ts)(T)>
+    template <typename T, typename E, T (*f)(T, T), T (*g)(T, E), E (*h)(E, E), T (*ts)(T)>
     struct LazyReversibleRBST : RBSTBase<LazyReversibleRBSTNode<T, E>> {
         using Node = LazyReversibleRBSTNode<T, E>;
         using base = RBSTBase<LazyReversibleRBSTNode<T, E>>;
         using base::merge;
         using base::split;
         using typename base::Ptr;
-
         LazyReversibleRBST() = default;
-
         void toggle(Ptr t) {
             if (!t) return;
             swap(t->l, t->r);
             t->sum = ts(t->sum);
             t->rev ^= true;
         }
-
         T fold(Ptr &t, int a, int b) {
             auto x = split(t, a);
             auto y = split(x.second, b - a);
@@ -117,14 +102,12 @@ namespace insertable_reversible_array_impl {
             t = merge(x.first, merge(y.first, y.second));
             return ret;
         }
-
         void reverse(Ptr &t, int a, int b) {
             auto x = split(t, a);
             auto y = split(x.second, b - a);
             toggle(y.first);
             t = merge(x.first, merge(y.first, y.second));
         }
-
         void apply(Ptr &t, int a, int b, const E &e) {
             auto x = split(t, a);
             auto y = split(x.second, b - a);
@@ -134,7 +117,6 @@ namespace insertable_reversible_array_impl {
 
     protected:
         inline T sum(const Ptr t) const { return t ? t->sum : T(); }
-
         Ptr update(Ptr t) override {
             push(t);
             t->cnt = 1;
@@ -143,7 +125,6 @@ namespace insertable_reversible_array_impl {
             if (t->r) t->cnt += t->r->cnt, t->sum = f(t->sum, t->r->sum);
             return t;
         }
-
         void push(Ptr t) override {
             if (t->rev) {
                 if (t->l) toggle(t->l);
@@ -156,28 +137,30 @@ namespace insertable_reversible_array_impl {
                 t->lazy = E();
             }
         }
-
         void propagate(Ptr t, const E &x) {
             t->lazy = h(t->lazy, x);
             t->key = g(t->key, x);
             t->sum = g(t->sum, x);
         }
     };
-    ll op(ll a, ll b) { return a + b; }
-    ll e(ll a) { return a; }
+    template <typename T>
+    T op(T a, T b) { return a + b; }
+    template <typename T>
+    T e(T a) { return a; }
 };  // namespace insertable_reversible_array_impl
 
+template <typename T>
 struct insertable_reversible_array {
-    using node = insertable_reversible_array_impl::LazyReversibleRBSTNode<ll, ll>;
+    using node = insertable_reversible_array_impl::LazyReversibleRBSTNode<T, T>;
     using node_ptr = node *;
     node_ptr root;
     insertable_reversible_array_impl::LazyReversibleRBST<
-        ll,
-        ll,
-        insertable_reversible_array_impl::op,
-        insertable_reversible_array_impl::op,
-        insertable_reversible_array_impl::op,
-        insertable_reversible_array_impl::e>
+        T,
+        T,
+        insertable_reversible_array_impl::op<T>,
+        insertable_reversible_array_impl::op<T>,
+        insertable_reversible_array_impl::op<T>,
+        insertable_reversible_array_impl::e<T>>
         rbst;
     int _size = 0;
     int size() {
@@ -185,17 +168,15 @@ struct insertable_reversible_array {
     }
     insertable_reversible_array() = default;
     insertable_reversible_array(int n) {
-        vector<ll> init(n);
+        vector<T> init(n);
         root = rbst.build(init);
         _size = n;
     }
-    template <typename T>
     insertable_reversible_array(const vector<T> &init) {
-        vector<ll>_init(init.begin(), init.end());
-        root = rbst.build(_init);
+        root = rbst.build(init);
         _size = init.size();
     }
-    void insert(int k, ll x) {
+    void insert(int k, T x) {
         assert(0 <= k && k <= _size);
         _size++;
         rbst.insert(root, k, x);
@@ -205,11 +186,11 @@ struct insertable_reversible_array {
         _size--;
         rbst.erase(root, k);
     }
-    ll get(int k) {
+    T get(int k) {
         assert(0 <= k && k < _size);
         return rbst.fold(root, k, k + 1);
     }
-    void set(int k, ll x) {
+    void set(int k, T x) {
         assert(0 <= k && k < _size);
         erase(k);
         insert(k, x);
--- data_structure/insrev.md
+++ data_structure/insrev.md
@@ -2,27 +2,27 @@
 
 # [Insertable Reversible Array](./insrev.hpp)
 
-`insertable_reversible_array(int n)`
-`insertable_reversible_array(vector<T> a)`
+`insertable_reversible_array<T>(int n)`
+`insertable_reversible_array<T>(vector<T> a)`
 - Insertable Reversible Array は挿入・削除・区間反転可能な配列である。
 
-`void insert(i, x)`
+`void insert(int i, T x)`
 - $i$ 番目に $x$ を挿入する。
 - $O(\log n)$
 
-`void erase(i)`
+`void erase(int i)`
 - $i$ 番目の要素を削除する。
 - $O(\log n)$
 
-`void reverse(i, j)`
+`void reverse(int i, int j)`
 - 区間 $[i, j)$ の要素を反転する。
 - $O(\log n)$
 
-`ll get(i)`
+`T get(i)`
 - $i$ 番目の要素を取得する。
 - $O(\log n)$
 
-`void set(i, x)`
+`void set(int i, T x)`
 - $i$ 番目の要素を $x$ に変更する。
 - $O(\log n)$
 
