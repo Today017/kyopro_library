2f94d6beb158600a6580ee63c37b5e6b7a0c699a
Today017 <yesterday998244353@gmail.com>
2024-07-07 06:44:42 +0900

    浮動小数点数に対する二分探索を追加

--- algorithm/bins.hpp
+++ algorithm/bins.hpp
@@ -12,3 +12,16 @@ T my_binary_search(T ok, T ng, Judge judge) {
     }
     return ok;
 }
+
+template <typename T, typename Judge>
+T my_binary_search_iteration(T ok, T ng, Judge judge, int count = 100) {
+	while (count--) {
+        T mid = (ok + ng) / 2;
+        if (judge(mid)) {
+            ok = mid;
+        } else {
+            ng = mid;
+        }
+    }
+    return ok;
+}
