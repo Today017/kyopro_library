2ff2afdaa2a6ad87c8c9e08f528134dfcb657cce
Today017 <yesterday998244353@gmail.com>
2024-07-07 06:45:53 +0900

    [Add] 木の直径

---
+++ graph/tree/diameter.hpp
@@ -0,0 +1,35 @@
+#include "../../../kyopro_library/template.hpp"
+#include "../../../kyopro_library/graph/sp/dij.hpp"
+
+pair<vector<int>, ll> tree_diameter_path(const vector<vector<pair<int, ll>>>& G) {
+    int N = G.size();
+    vector<ll> dist = dijkstra(G);
+    int s = max_element(dist.begin(), dist.end()) - dist.begin();
+    dist = dijkstra(G, s);
+    int t = max_element(dist.begin(), dist.end()) - dist.begin();
+    vector<int> path;
+    int now = t;
+    while (now != s) {
+        path.push_back(now);
+        for (auto [nxt, cost] : G[now]) {
+            if (dist[now] == dist[nxt] + cost) {
+                now = nxt;
+                break;
+            }
+        }
+    }
+    path.push_back(s);
+    ll diameter = dist[t];
+    return {path, diameter};
+}
+
+pair<vector<int>, ll> tree_diameter_path(const vector<vector<int>>& G) {
+    int N = G.size();
+    vector<vector<pair<int, ll>>> H(N);
+    for (int i = 0; i < N; i++) {
+        for (int j : G[i]) {
+            H[i].push_back({j, 1});
+        }
+    }
+    return tree_diameter_path(H);
+}
