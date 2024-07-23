fab2aa80df594339d1e502f44aa1357b1b419a34
Today017 <yesterday998244353@gmail.com>
2024-07-23 10:36:29 +0900

    [Fix] Timer

--- others/timer.hpp
+++ others/timer.hpp
@@ -1,18 +1,18 @@
 #pragma once
 #include "../../kyopro_library/template.hpp"
 
-struct timer {
+struct Timer {
     chrono::high_resolution_clock::time_point start;
     chrono::high_resolution_clock::time_point time_limit;
-    timer(ll time_limit_msec = 5000) : start(chrono::high_resolution_clock::now()) {
-        time_limit = start + chrono::microseconds((ll)(time_limit_msec * 1e3));
-    }
-    inline ll get() {
+    Timer(ll time_limit_msec = 5000) {
+		start = chrono::high_resolution_clock::now();
+		time_limit = start + chrono::milliseconds(time_limit_msec * 1000);
+	}
+    inline ll get() const {
         auto now = chrono::high_resolution_clock::now();
         return chrono::duration_cast<chrono::milliseconds>(now - start).count();
     }
-    inline bool check() {
-        auto now = chrono::high_resolution_clock::now();
-        return now < time_limit;
+    inline bool check() const {
+        return chrono::high_resolution_clock::now() < time_limit;
     }
 };
--- others/timer.md
+++ others/timer.md
@@ -2,31 +2,11 @@
 
 # [Timer](./timer.hpp)
 
-`timer(ll tlm)`
-- `tlm` msec を制限時間とした timer を定義し、時間の計測を開始する。
+`Timer(ll tlm)`
+- `tlm` msec を制限時間とした Timer を定義し、時間の計測を開始する。
 
 `ll get()`
 - 今が計測開始から何 msec 後かを返す。
 
 `bool check()`
 - 今が制限時間内であるか否かを返す。
-
----
-
-```cpp
-struct timer {
-    chrono::high_resolution_clock::time_point start;
-    chrono::high_resolution_clock::time_point time_limit;
-    timer(ll time_limit_msec = 5000) : start(chrono::high_resolution_clock::now()) {
-        time_limit = start + chrono::microseconds((ll)(time_limit_msec * 1e3));
-    }
-    inline ll get() {
-        auto now = chrono::high_resolution_clock::now();
-        return chrono::duration_cast<chrono::milliseconds>(now - start).count();
-    }
-    inline bool check() {
-        auto now = chrono::high_resolution_clock::now();
-        return now < time_limit;
-    }
-};
-```
