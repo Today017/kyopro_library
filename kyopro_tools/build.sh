1fb40dea545ed0d08bdb5c23c921dbd9e312dc3d
Today017 <yesterday998244353@gmail.com>
2024-08-04 17:49:34 +0900

    [Update] build.sh

--- kyopro_tools/build.sh
+++ kyopro_tools/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# 環境側でやること
+# 1. GCCのインストール
+#   - C++20が使える
+#   - #include <bits/stdc++.h> が使える
+# 2. Python3のインストール
+# 3. ~/.vimrc を設定する
+# 4. ~/.bashrc / ~/.zshrc で pbcopy をコピー用のエイリアスとして設定する
+
 # chmod 755 build.sh
 
 git clone https://github.com/Today017/kyopro_library.git
