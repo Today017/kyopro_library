a2a0768e059a78ed414bae5d1afeb0356048993b
Today017 <yesterday998244353@gmail.com>
2024-10-27 11:26:20 +0900

    [Update] 転倒数のアルゴリズムを分割統治法にすることで座標圧縮を不要に

--- algorithm/inversion_number.hpp
+++ algorithm/inversion_number.hpp
@@ -1,24 +1,24 @@
 #include "../../kyopro_library/template.hpp"
 
 ll inversionNumber(const vector<int> &a) {
-    using T=vector<int>;
-    ll ret=0;
-    auto Merge=[&](const T &a,const T &b){
-        int n=a.size();
-        int r=0;
-        for(int l=0;l<n;l++){
-            while(r<n&&b[r]<a[l])r++;
-            ret+=r;
-        }
-        T c(2*n);
-        merge(a.begin(),a.end(),b.begin(),b.end(),c.begin());
-        return c;
-    };
+	using T=vector<int>;
+	ll ret=0;
+	auto Merge=[&](const T &a,const T &b){
+		int n=a.size();
+		int r=0;
+		for(int l=0;l<n;l++){
+			while(r<n&&b[r]<a[l])r++;
+			ret+=r;
+		}
+		T c(2*n);
+		merge(a.begin(),a.end(),b.begin(),b.end(),c.begin());
+		return c;
+	};
 
-    int n=a.size(),n2=1;
-    while(n2<n)n2<<=1;
-    vector<T> node(n2<<1,T(1,INF));
-    for(int i=0;i<n;i++)node[i+n2][0]=a[i];
-    for(int i=n2-1;i>0;i--)node[i]=Merge(node[i<<1],node[i<<1|1]);
-    return ret;
+	int n=a.size(),n2=1;
+	while(n2<n)n2<<=1;
+	vector<T> node(n2<<1,T(1,INF));
+	for(int i=0;i<n;i++)node[i+n2][0]=a[i];
+	for(int i=n2-1;i>0;i--)node[i]=Merge(node[i<<1],node[i<<1|1]);
+	return ret;
 }
