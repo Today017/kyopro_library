14448b24ae16343d58938cd1a785e3f5199e1159
Today017 <yesterday998244353@gmail.com>
2024-12-01 18:14:25 +0900

    [Fix] Segtree Dual

--- data_structure/segtd.hpp
+++ data_structure/segtd.hpp
@@ -1,33 +1,33 @@
 #pragma once
 #include"../../kyopro_library/template.hpp"
 
-template<typename Operator>
+template<typename CommutativeOperator>
 struct SegTreeDual{
-	using Type=typename Operator::Type;
+	using Type=typename CommutativeOperator::Type;
 	SegTreeDual()=default;
 	SegTreeDual(int n){
 		this->n=n;
-		dat.assign(n<<1,Operator::id());
+		dat.assign(n<<1,CommutativeOperator::id());
 	}
 	SegTreeDual(const vector<Type>&v){
 		this->n=v.size();
-		dat.assign(n<<1,Operator::id());
+		dat.assign(n<<1,CommutativeOperator::id());
 		for(int i=0;i<n;i++)dat[i+n]=v[i];
-		for(int i=n-1;i>0;i--)dat[i]=Operator::op(dat[i<<1],dat[i<<1|1]);
+		for(int i=n-1;i>0;i--)dat[i]=CommutativeOperator::op(dat[i<<1],dat[i<<1|1]);
 	}
 	void apply(int l,int r,Type x){
 		l+=n,r+=n;
 		while(l<r){
-			if(l&1)dat[l]=Operator::op(dat[l],x),l++;
-			if(r&1)r--,dat[r]=Operator::op(dat[r],x);
+			if(l&1)dat[l]=CommutativeOperator::op(dat[l],x),l++;
+			if(r&1)r--,dat[r]=CommutativeOperator::op(dat[r],x);
 			l>>=1,r>>=1;
 		}
 	}
 	Type get(int i){
 		i+=n;
-		Type ret=Operator::id();
+		Type ret=CommutativeOperator::id();
 		while(i){
-			ret=Operator::op(ret,dat[i]);
+			ret=CommutativeOperator::op(ret,dat[i]);
 			i>>=1;
 		}
 		return ret;
