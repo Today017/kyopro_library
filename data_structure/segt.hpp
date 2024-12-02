fd9c0c135d6aebbafe721ce461b435b7ae5e4fb3
Today017 <yesterday998244353@gmail.com>
2024-12-03 05:01:07 +0900

    [Update]

--- data_structure/segt.hpp
+++ data_structure/segt.hpp
@@ -41,7 +41,7 @@ struct SegTree{
 		assert(f(Monoid::id()));
 		l+=n;
 		int r=n+n;
-		vector<int> cand_l,cand_r;
+		vector<int>cand_l,cand_r;
 		while(l<r){
 			if(l&1)cand_l.push_back(l++);
 			if(r&1)cand_r.push_back(--r);
@@ -71,10 +71,10 @@ struct SegTree{
 	template<typename F>
 	int find_leftest(int r,F f){
 		assert(f(Monoid::id()));
-        if(r==0)return 0;
+		if(r==0)return 0;
 		r+=n;
 		int l=n;
-		vector<int> cand_l,cand_r;
+		vector<int>cand_l,cand_r;
 		while(l<r){
 			if(l&1)cand_l.push_back(l++);
 			if(r&1)cand_r.push_back(--r);
@@ -94,7 +94,7 @@ struct SegTree{
 						val=Monoid::op(dat[i],val);
 						i^=1;
 					}
-                }
+				}
 				return i-n+1;
 			}
 		}
@@ -116,3 +116,5 @@ struct RangeMax{using Type=struct SegTree<MaxMonoid<T,min_value>>;};
 
 template<typename T>
 struct RangeSum{using Type=struct SegTree<SumMonoid<T>>;};
+
+
