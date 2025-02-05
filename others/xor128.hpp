a025b625044700fbe17da24b66e542195d4ce8f2
Today017 <yesterday998244353@gmail.com>
2025-02-06 04:05:44 +0900

    [Update] xor128

--- others/xor128.hpp
+++ others/xor128.hpp
@@ -1,27 +1,19 @@
+#pragma once
 #include "../../kyopro_library/template.hpp"
 
-struct Random {
-    Random(uint64_t seed = time(0)) {
-        x = 123456789;
-        y = 362436069;
-        z = 521288629;
-        w = 88675123;
-        x ^= seed;
-        y ^= (seed << 8);
-        z ^= (seed << 16);
-        w ^= (seed << 24);
+uint64_t xor128() {
+    static bool flag = false;
+    static uint64_t x = 123456789, y = 362436069, z = 521288629, w = 88675123;
+    if (!flag) {
+        w = time(0);
+        flag = true;
     }
-    inline uint64_t operator()(uint64_t n = INFL) {
-        uint64_t t = x ^ (x << 11);
-        x = y;
-        y = z;
-        z = w;
-        uint64_t ret = (w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)));
-        return ret % n;
-    }
-    inline uint64_t operator()(uint64_t l, uint64_t r) { return operator()(r - l) + l; }
-    inline double prob() { return (double)operator()(1LL << 32) / (1LL << 32); }
-
-private:
-    uint64_t x, y, z, w;
-};
+    uint64_t t = x ^ (x << 11);
+    x = y;
+    y = z;
+    z = w;
+    return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+}
+uint64_t xor128(uint64_t n) { return xor128() % n; }
+uint64_t xor128(uint64_t l, uint64_t r) { return xor128(r - l) + l; } // [l, r)
+double xor128_prob() { return (double)xor128() / (1LL << 32); }
