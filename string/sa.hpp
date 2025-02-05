dbf3e3d2010e5a0b4bdfe02da6640b758e80d9f0
Today017 <yesterday998244353@gmail.com>
2025-02-06 04:06:39 +0900

    [Add] Suffix Array

---
+++ string/sa.hpp
@@ -0,0 +1,46 @@
+#include"../../kyopro_library/template.hpp"
+
+template<int C=256>
+vector<int> suffixArray(string s){
+	s.push_back('$');
+	int n=s.size();
+	vector<int>p(n),c(n),cnt(max(n,C),0);
+	//p[i]:=辞書順i番目のインデックス
+	//c[i]:=インデックスiの部分文字列の同値類
+
+	//2^0,2^1,...,2^k,... をやる
+	//k=0
+	for(int i=0;i<n;i++)cnt[s[i]]++;
+	for(int i=1;i<(int)cnt.size();i++)cnt[i]+=cnt[i-1];
+	//辞書順=累積和の小さい順になる
+	for(int i=0;i<n;i++)p[--cnt[s[i]]]=i;
+	//同値類を計算する
+	c[p[0]]=0;
+	for(int i=1;i<n;i++){
+		c[p[i]]=c[p[i-1]];
+		if(s[p[i]]!=s[p[i-1]])c[p[i]]++;
+	}
+	debug(p);
+
+	vector<int>np(n),nc(n);
+	for(int k=0;(1<<k)<n;k++){
+		//kを使って、(c[i],c[i+2^k])でソート、p[i]-=2^kでできる
+		//c[i+2^k]でソート
+		for(int i=0;i<n;i++)np[i]=p[i]-(1<<k),(np[i]+=n)%=n;
+		debug(np);
+		ranges::fill(cnt,0);
+		for(int i=0;i<n;i++)cnt[c[np[i]]]++;
+		for(int i=1;i<(int)cnt.size();i++)cnt[i]+=cnt[i-1];
+		for(int i=n-1;i>=0;i--)p[--cnt[c[np[i]]]]=np[i];
+		//同値類
+		nc[p[0]]=0;
+		for(int i=1;i<n;i++){
+			nc[p[i]]=nc[p[i-1]];
+			if(c[p[i]]!=c[p[i-1]]||c[(p[i]+(1<<k))%n]!=c[(p[i-1]+(1<<k))%n])nc[p[i]]++;
+		}
+		swap(c,nc);
+	}
+
+	p.erase(p.begin());
+	return p;
+}
