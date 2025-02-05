99164be6ce38e415204b645020d4189c0c835749
Today017 <yesterday998244353@gmail.com>
2025-02-06 04:06:20 +0900

    [Update] proot, rho

--- math/rho.hpp
+++ math/rho.hpp
@@ -9,10 +9,7 @@ vector<pair<ll,ll>>primeFactorize(ll n){
 	auto find_factor=[](auto&&find_factor,ll n)->ll{
 		__int128_t m=(ll)pow(n,0.125)+1;
 		auto _gcd=[](__int128_t a,__int128_t b){
-			while(a){
-				b%=a;
-				swap(a,b);
-			}
+			while(a)b%=a,swap(a,b);
 			return b;
 		};
 		auto _abs=[](__int128_t x){return x<0?-x:x;};
@@ -21,30 +18,17 @@ vector<pair<ll,ll>>primeFactorize(ll n){
 			__int128_t y=0,r=1,q=1,g=1,k=0,x=0,ys=0;
 			while(g==1){
 				x=y;
-				while(k<r*3/4){
-					y=f(y);
-					k++;
-				}
-				ll cnt=0;
+				while(k<r*3/4)y=f(y),k++;
 				while(k<r&&g==1){
 					ys=y;
-					for(int i=0;i<min(m,r-k);i++){
-						y=f(y);
-						q=q*_abs(x-y)%n;
-					}
-					g=_gcd(q,n);
-					k+=m;
+					for(int i=0;i<min(m,r-k);i++)y=f(y),q=q*_abs(x-y)%n;
+					g=_gcd(q,n),k+=m;
 				}
-				k=r;
-				r*=2;
+				k=r,r*=2;
 			}
 			if(g==n){
-				g=1;
-				y=ys;
-				while(g==1){
-					y=f(y);
-					g=_gcd(_abs(x-y),n);
-				}
+				g=1,y=ys;
+				while(g==1)y=f(y),g=_gcd(_abs(x-y),n);
 			}
 			if(g<n){
 				if(primalityTest(g))return g;
@@ -54,29 +38,21 @@ vector<pair<ll,ll>>primeFactorize(ll n){
 		}
 		return 0;
 	};
-
 	map<ll,ll>mp;
 	ll i=2;
 	while(i*i<=n){
 		ll k=0;
-		while(n%i==0){
-			n/=i;
-			k++;
-		}
+		while(n%i==0)n/=i,k++;
 		if(k)mp[i]=k;
 		i+=i%2+1;
 		if(i==101&&n>=(1ll<<20)){
 			while(n>1){
 				if(primalityTest(n)){
-					mp[n]=1;
-					n=1;
+					mp[n]=1,n=1;
 				}else{
 					ll j=find_factor(find_factor,n);
 					k=0;
-					while(n%j==0){
-						n/=j;
-						k++;
-					}
+					while(n%j==0)n/=j,k++;
 					mp[j]=k;
 				}
 			}
