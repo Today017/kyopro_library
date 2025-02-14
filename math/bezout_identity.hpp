4d40c1972a206f224dde38104eb276343760dfc5
Today017 <yesterday998244353@gmail.com>
2025-02-14 12:44:08 +0900

    [Fix] Bezout

--- math/bezout_identity.hpp
+++ math/bezout_identity.hpp
@@ -1,36 +1,37 @@
 #include"../../kyopro_library/template.hpp"
+#include"../../kyopro_library/math/extgcd.hpp"
 
+// 一次不定方程式 ax+by=c を解く
+// verify: https://atcoder.jp/contests/abc340/submissions/62495050
+/*
+	g=gcd(a,b) とする。extGCD(a,b) によって、ax'+by'=g を解く。
+	両辺を c/g 倍し x=x'*(c/g),y=y'*(c/g) とすると、ax+by=c を満たす。c が g で割り切れない場合、解は存在しない。
+	ax+by=c の両辺を g で割ると、(a/g)*x + (b/g)*y = c/g となる。方程式の一般解を X,Y とおくと、(a/g)*(X-x) + (b/g)*(Y-y) = 0 を満たす。
+	a/g と b/g は互いに素なので X-x は b/g の倍数であり、X=(b/g)*t+x となる。
+	これを再代入すると、Y=-(a/g)*t+y を得る。
+*/
 struct BezoutIdentity{
 	ll a,b,c,g;
 	ll X,Y;
+	// ax+by=c
 	BezoutIdentity(ll a,ll b,ll c){
 		this->a=a;
 		this->b=b;
 		this->c=c;
 	}
-	tuple<ll,ll,ll>extgcd(ll a,ll b){
-		if(b==0)return make_tuple(a,1,0);
-		ll q=a/b,r=a%b;
-		auto[g,s,t]=extgcd(b,r);
-		ll x=t;
-		ll y=s-q*t;
-		return make_tuple(g,x,y);
-	}
+	// 解が存在するか否かを返す
 	bool build(){
-		auto[g,X,Y]=extgcd(abs(a),abs(b));
+		auto[g,X,Y]=extGCD(abs(a),abs(b));
 		if(c%g!=0)return false;
-		this->g=g;
-		this->X=X;
-		this->Y=Y;
+		this->g=g,this->X=X,this->Y=Y;
 		if(a<0)this->X=-this->X;
 		if(b<0)this->Y=-this->Y;
-		this->X*=c/g;
-		this->Y*=c/g;
+		this->X*=c/g,this->Y*=c/g;
 		return true;
 	}
+	// 一般解を返す
 	pair<ll,ll>general_solution(ll t=0){
-		ll x=b/g*t+X;
-		ll y=-a/g*t+Y;
-		return make_pair(x,y);
+		ll x=b/g*t+X,y=-a/g*t+Y;
+		return {x,y};
 	}
 };
