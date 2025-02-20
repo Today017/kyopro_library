56ee747a45c37d8cf38b91b692fe530e12f0e01f
Today017 <yesterday998244353@gmail.com>
2025-02-21 07:04:39 +0900

    [Add] マクロ

---
+++ mac.hpp
@@ -0,0 +1,6 @@
+const char nl = '\n';
+#define ALL(x) (x).begin(), (x).end()
+#define FAST ios::sync_with_stdio(false), cin.tie(nullptr);
+#define chmax(a, b) (a) = max(a, b)
+#define chmin(a, b) (a) = min(a, b)
+#define unique(x) sort(ALL(x)), (x).erase(unique(ALL(x)), (x).end())
