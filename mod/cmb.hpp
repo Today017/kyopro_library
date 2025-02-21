7eb01b06e230d9264d8f1d95d4c4201b706508de
Today017 <yesterday998244353@gmail.com>
2025-02-21 09:09:17 +0900

    [Update] combination

--- mod/cmb.hpp
+++ mod/cmb.hpp
@@ -1,40 +1,29 @@
 #include"../../kyopro_library/template.hpp"
-#include"../../kyopro_library/mod/modint.hpp"
 
-// 二項係数
-// 階乗
-template<typename mint=Mod998>
-mint factrial(int n){
-	static vector<mint>fac;
-	if((int)fac.size()<=n){
-		if(fac.empty())fac.push_back(1);
-		for(int i=fac.size();i<=n;i++)fac.push_back(fac.back()*i);
+template<typename T>
+struct Combinatorics{
+	Combinatorics(int n){
+		fac=vector<T>(n+1);
+		finv=vector<T>(n+1);
+		fac[0]=1;
+		for(int i=1;i<=n;i++)fac[i]=fac[i-1]*i;
+		finv[n]=fac[n].inv();
+		for(int i=n;i>=1;i--)finv[i-1]=finv[i]*i;
 	}
-	return fac[n];
-}
-
-// 階乗逆元
-template<typename mint=Mod998>
-mint factinv(int n){
-	static vector<mint>minv,finv;//逆元, 階乗逆元
-	if((int)finv.size()<=n){
-		//https://drken1215.hatenablog.com/entry/2018/06/08/210000
-		if(minv.empty())minv.push_back(0),minv.push_back(1);
-		for(int i=minv.size();i<=n;i++)minv.push_back(-minv[mint::get_mod()%i]*(mint::get_mod()/i));
-		if(finv.empty())finv.push_back(1);
-		for(int i=finv.size();i<=n;i++)finv.push_back(finv.back()*minv[i]);
+	T comb(ll n,ll r){
+		if(n<0||r<0||n-r<0)return 0;
+		return fac[n]*finv[r]*finv[n-r];
 	}
-	return finv[n];
-}
+	T perm(ll n,ll r){
+		if(n<0||r<0||n-r<0)return 0;
+		return fac[n]*finv[n-r];
+	}
+	T fact(int n){return fac[n];}
+	T factinv(int n){return finv[n];}
+	T operator()(ll n,ll r){return comb(n,r);}
 
-template<typename mint=Mod998>
-mint comb(int n,int r){
-	if(n<0||r<0||n-r<0)return 0;
-	return factrial<mint>(n)*factinv<mint>(r)*factinv<mint>(n-r);
-}
+private:
+	vector<T>fac,finv;
+};
 
-template<typename mint=Mod998>
-mint perm(int n,int r){
-	if(n<0||r<0||n-r<0)return 0;
-	return factrial<mint>(n)*factinv<mint>(n-r);
-}
+
