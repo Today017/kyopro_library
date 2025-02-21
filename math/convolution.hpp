ab4eb6f2ec36d57c7639edf0fd9a069cdc5d550e
Today017 <yesterday998244353@gmail.com>
2025-02-21 10:00:50 +0900

    [Update] convolution

--- math/convolution.hpp
+++ math/convolution.hpp
@@ -1,8 +1,9 @@
 #include"../../kyopro_library/template.hpp"
 
-vector<complex<double>>fft(vector<complex<double>>a,bool inv=false){
-	int n=a.size();
-	int h=0;
+// 高速フーリエ変換 O(N log(N))
+// f(x) = Σ a[i]x^i, w^N = 1 とすると、F(t) = Σ f(w^i)t^i の各係数を返す。
+vector<complex<double>>FFT(vector<complex<double>>a,bool inv=false){
+	int n=a.size(),h=0;
 	while((1<<h)<n)h++;
 	for(int i=0;i<n;i++){
 		int j=0;
@@ -18,29 +19,40 @@ vector<complex<double>>fft(vector<complex<double>>a,bool inv=false){
 			complex<double>w=polar(1.0,(2.0*M_PI)/(2.0*b)*j*(inv?1:-1));
 			//ブロックサイズ2bだけずらしながら計算
 			for(int k=0;k<n;k+=b*2){
-				complex<double>s=a[j+k];
-				complex<double>t=a[j+k+b]*w;
-				a[j+k]=s+t;
-				a[j+k+b]=s-t;
+				complex<double>s=a[j+k],t=a[j+k+b]*w;
+				a[j+k]=s+t,a[j+k+b]=s-t;
 			}
 		}
 	}
-	if(inv)for(int i=0;i<n;i++)a[i]/=n;
+	if(inv){
+		for(int i=0;i<n;i++)a[i]/=n;
+	}
 	return a;
 }
 
-vector<double>convolve(vector<double>a,vector<double>b){
+vector<double>convolve(const vector<double>&a,const vector<double>&b){
 	int n=1;
-	while(n<(int)a.size()+(int)b.size()-1)n<<=1;
+	while(n<(int)a.size()+(int)b.size()-1)n*=2;
 	vector<complex<double>>fa(n),fb(n);
 	for(int i=0;i<(int)a.size();i++)fa[i]=a[i];
 	for(int i=0;i<(int)b.size();i++)fb[i]=b[i];
-	fa=fft(fa);
-	fb=fft(fb);
+	// fa, fb を高速フーリエ変換
+	fa=FFT(fa),fb=FFT(fb);
+	// fa' * fb' を計算
 	for(int i=0;i<n;i++)fa[i]*=fb[i];
-	fa=fft(fa,true);
+	// fa' * fb' を逆高速フーリエ変換し、fa * fb の係数を得る
+	fa=FFT(fa,true);
 	vector<double>ret(n);
 	for(int i=0;i<n;i++)ret[i]=fa[i].real();
-	while((int)ret.size()>(int)a.size()+(int)b.size()-1)ret.pop_back();
+	return ret;
+}
+
+vector<ll>convolve(const vector<ll>&a,const vector<ll>&b){
+	vector<double>da(a.size()),db(b.size());
+	for(int i=0;i<(int)a.size();i++)da[i]=a[i];
+	for(int i=0;i<(int)b.size();i++)db[i]=b[i];
+	vector<double>ab=convolve(da,db);
+	vector<ll>ret(ab.size());
+	for(int i=0;i<(int)ret.size();i++)ret[i]=(ll)floor(ab[i]+0.5);
 	return ret;
 }
