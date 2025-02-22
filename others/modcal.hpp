50e2b13ca1a5732cc2fa680ec6371c91ca976a93
Today017 <yesterday998244353@gmail.com>
2025-02-22 18:43:28 +0900

    [Update] コメント追加

--- others/modcal.hpp
+++ others/modcal.hpp
@@ -1,18 +1,21 @@
 #pragma once
 #include "../../kyopro_library/template.hpp"
 
+// modPow(x, n, m) : x^n (mod m) を返す
+// O(log(m))
 template <typename T = ll>
 T modPow(T x, T n, T mod) {
-    ll ret = 1;
-    if (typeid(T) == typeid(ll) && mod > INF * 2) return modPow<__int128_t>(x, n, mod);
-    while (n > 0) {
-        if (n & 1) ret = ret * x % mod;
-        x = x * x % mod;
-        n >>= 1;
-    }
-    return ret;
+	ll ret = 1;
+	if (typeid(T) == typeid(ll) && mod > INF * 2) return modPow<__int128_t>(x, n, mod);
+	while (n > 0) {
+		if (n & 1) ret = ret * x % mod;
+		x = x * x % mod;
+		n >>= 1;
+	}
+	return ret;
 }
 
-ll modInv(ll x, ll mod) {
-    return modPow(x, mod - 2, mod);
-}
+// modInv(x, m) : x*y = 1 (mod m) なる y を返す
+// O(log(m))
+// ただし、m は素数
+ll modInv(ll x, ll mod) {return modPow(x, mod - 2, mod);}
