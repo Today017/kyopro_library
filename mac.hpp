abd0e60a0d9e06bdebbd202a6569be12753abf68
Today017 <yesterday998244353@gmail.com>
2025-02-24 10:53:09 +0900

    [Update] テンプレート

--- mac.hpp
+++ mac.hpp
@@ -1,6 +1,4 @@
-const char nl = '\n';
-#define ALL(x) (x).begin(), (x).end()
-#define FAST ios::sync_with_stdio(false), cin.tie(nullptr);
-#define chmax(a, b) (a) = max(a, b)
-#define chmin(a, b) (a) = min(a, b)
-#define unique(x) sort(ALL(x)), (x).erase(unique(ALL(x)), (x).end())
+#define ALL(x) (x).begin(),(x).end()
+#define IO ios::sync_with_stdio(false),cin.tie(nullptr);
+#define chmax(a,b)(a)=(a)<(b)?(b):(a)
+#define chmin(a,b)(a)=(a)<(b)?(a):(b)
--- template.hpp
+++ template.hpp
@@ -1,5 +1,9 @@
 #pragma once
 #include<bits/stdc++.h>
+#define ALL(x) (x).begin(),(x).end()
+#define IO ios::sync_with_stdio(false),cin.tie(nullptr);
+#define chmax(a,b)(a)=(a)<(b)?(b):(a)
+#define chmin(a,b)(a)=(a)<(b)?(a):(b)
 using namespace std;
 using ll=long long;
 const int INF=1e9+10;
