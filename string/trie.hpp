268af54bd6ce74b38d5fe2b338295de660e8854a
Today017 <yesterday998244353@gmail.com>
2025-04-28 04:06:30 +0000

    Trieの雛形修正

--- string/trie.hpp
+++ string/trie.hpp
@@ -6,16 +6,19 @@ struct Trie {
         int count_of_end=0,count_of_node=0;
         Node() { fill(ALL(to),-1); }
     };
+
     Trie(int len) {
         nodes.reserve(len);
         Node root;
         nodes.push_back(root);
     }
+
     void insert(const string& s) {
         int cur=0;
         for(const char& c:s) {
-            int&nxt=nodes[cur].to[c-'a'];
+            int nxt=nodes[cur].to[c-'a'];
             if(nxt==-1) {
+                nodes[cur].to[c-'a']=nodes.size();
                 nxt=nodes.size();
                 nodes.push_back(Node());
             }
@@ -24,19 +27,21 @@ struct Trie {
         }
         nodes[cur].count_of_end++;
     }
+
     int count(const string& s) {
         int cur=0,ret=0;
         for(const char& c:s) {
-            int&nxt=nodes[cur].to[c-'a'];
+            int nxt=nodes[cur].to[c-'a'];
             if(nxt==-1) return 0;
             cur=nxt;
             ret=nodes[cur].count_of_end;
         }
     }
+
     void erase(const string& s) {
         int cur=0;
         for(const char& c:s) {
-            int&nxt=nodes[cur].to[c-'a'];
+            int nxt=nodes[cur].to[c-'a'];
             if(nxt==-1) return;
             cur=nxt;
             nodes[cur].count_of_node--;
