5131895b073988bba5a5c6b81e73bac602c242aa
Today017 <yesterday998244353@gmail.com>
2025-05-09 00:11:28 +0000

    マクロを関数に修正

--- template.hpp
+++ template.hpp
@@ -4,13 +4,13 @@ using namespace std;
 
 #define ALL(x) (x).begin(),(x).end()
 #define IO ios::sync_with_stdio(false),cin.tie(nullptr);
-#define LB(v, x) (ll)(lower_bound(ALL(v),x)-(v).begin())
-#define UQ(v) sort(ALL(v)),(v).erase(unique(ALL(v)),v.end())
 #define REP(i, n) for(ll i=0; i<(ll)(n); i++)
 #define FOR(i, a, b) for(ll i=(ll)(a); (a)<(b) ? i<(b) : i>(b); i+=((a)<(b) ? 1 : -1))
-#define chmax(a, b) ((a)<(b) ? ((a)=(b), 1) : 0)
-#define chmin(a, b) ((a)>(b) ? ((a)=(b), 1) : 0)
 
+template<typename T> int LB(vector<T> v, T x) { return lower_bound(ALL(v),x)-(v).begin(); }
+template<typename T> int UQ(T& v) { sort(ALL(v)); v.erase(unique(ALL(v)),v.end()); return v.size(); }
+template<typename T> bool chmax(T &a, T b) { return a<b ? a=b, true : false; }
+template<typename T> bool chmin(T &a, T b) { return a>b ? a=b, true : false; }
 template<typename T> using rpriority_queue=priority_queue<T,vector<T>,greater<T>>;
 using ll=long long; const int INF=1e9+10; const ll INFL=4e18;
 using ld=long double; using ull=uint64_t; using LL=__int128_t;
