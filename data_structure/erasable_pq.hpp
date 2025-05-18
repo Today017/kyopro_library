de4561b5ede00fd4fddef47ca11c85446a0f4628
Today017 <yesterday998244353@gmail.com>
2025-05-18 04:26:36 +0000

    削除可能PQ 追加

---
+++ data_structure/erasable_pq.hpp
@@ -0,0 +1,45 @@
+#include"../../kyopro_library/template.hpp"
+
+/// @brief 削除可能な優先度付きキュー
+/// @tparam MAX trueのとき、最大値を返す
+template<typename T, bool MAX=true>
+struct ErasablePQ {
+    priority_queue<T> pq, pq2;
+    int siz=0;
+
+    ErasablePQ()=default;
+
+    /// @brief x を追加する
+    void push(int x) {
+        if(!MAX) x=-x;
+        pq.push(x);
+        siz++;
+    }
+
+    /// @brief x を削除する
+    void erase(int x) {
+        if(!MAX) x=-x;
+        pq2.push(x);
+        siz--;
+    }
+
+    /// @brief 最大値を返す
+    T top() {
+        while(!pq.empty()) {
+            if(pq2.empty()) return pq.top();
+            if(pq.top()==pq2.top()) {
+                pq.pop();
+                pq2.pop();
+            } else {
+                return pq.top();
+            }
+        }
+
+        cout<<"ErasablePQ::top() : empty"<<endl;
+        exit(1);
+        return T();
+    }
+
+    /// @brief 現在のサイズを返す
+    int size() { return siz; }
+};
