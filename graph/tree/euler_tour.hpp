0de1edc3ba5953660d6047d2d72358c9f05d15b8
Today017 <yesterday998244353@gmail.com>
2025-05-18 04:26:44 +0000

    オイラーツアー 追加

---
+++ graph/tree/euler_tour.hpp
@@ -0,0 +1,21 @@
+#include"../../../kyopro_library/template.hpp"
+
+/// @brief 木 g をオイラーツアーする
+/// @brief `(行きがけ順, 深さ, 入力時刻, 出力時刻)` を返す
+/// @details 部分木のクエリを列のに対する区間クエリとして扱えるようになる
+/// @details 例えば、頂点 v の部分木はオイラーツアー後の列の区間 `[in[v], out[v])` に対応する
+tuple<VI,VI,VI,VI> EulerTour(const VVI& g) {
+    int n=g.size();
+    VI tour,depth(n),in(n),out(n);
+    int time=0;
+    auto dfs=[&](auto&& dfs, int now, int pre, int d)-> void {
+        depth[now]=d;
+        in[now]=time++;
+        tour.push_back(now);
+        for(int nxt:g[now]) if(nxt!=pre) dfs(dfs,nxt,now,d+1);
+        out[now]=time;
+    };
+    dfs(dfs,0,-1,0);
+
+    return {tour,depth,in,out};
+}
