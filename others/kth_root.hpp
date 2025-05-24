4097fc7cb3343ce4f0fcfcc1d19ce9cac2434fc5
Today017 <yesterday998244353@gmail.com>
2025-05-24 18:52:25 +0000

    コンパイルエラー解消

--- others/kth_root.hpp
+++ others/kth_root.hpp
@@ -4,9 +4,8 @@
 /// @brief n　の k 乗根の切り捨てを返す
 ll KthRoot(ll n, ll k=2) {
     if(k==1) return n;
-    return (ll)BinarySearch<LL>(0,4e10,[&](LL x) {
-        LL tmp=1;
-        bool flag=true;
+    return (ll)BinarySearch<ll>(0,4e10,[&](ll x) {
+        lll tmp=1;
         REP(i,k) {
             if(tmp*x>n) return false;
             tmp*=x;
