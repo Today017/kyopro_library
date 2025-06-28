79f1b9eb8a2c7a9b5ebee00e02aef438e1edf588
Today017 <yesterday998244353@gmail.com>
2025-06-28 13:45:14 +0900

    ordered set系修正

--- data_structure/sorted_tree.hpp
+++ data_structure/sorted_tree.hpp
@@ -1,6 +1,7 @@
 #include"../../kyopro_library/template.hpp"
 
-#include<bits/extc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template<typename T>
 struct SortedTree:tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update> {
