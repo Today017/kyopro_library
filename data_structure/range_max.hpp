659594c3477bbf86495b0b98ac5b869b05b9c86c
Today017 <yesterday998244353@gmail.com>
2025-06-28 13:44:54 +0900

    RMQ追加

---
+++ data_structure/range_max.hpp
@@ -0,0 +1,21 @@
+#include"../../kyopro_library/template.hpp"
+
+struct RangeMax {
+    int n;
+    vector<ll> dat;
+    RangeMax(int m) { n=m; dat=vector<ll>(2*n,-INF); }
+    void set(int i, ll x) {
+        dat[i+=n]=x;
+        while(i>>=1) dat[i]=max(dat[i<<1],dat[i<<1|1]);
+    }
+    ll max(int l, int r) {
+        l+=n; r+=n;
+        ll ret=-INF;
+        while(l<r) {
+            if(l&1) chmax(ret,dat[l++]);
+            if(r&1) chmax(ret,dat[--r]);
+            l>>=1; r>>=1;
+        }
+        return ret;
+    }
+};
---
+++ data_structure/range_min.hpp
@@ -0,0 +1,21 @@
+#include"../../kyopro_library/template.hpp"
+
+struct RangeMin {
+    int n;
+    vector<ll> dat;
+    RangeMin(int m) { n=m; dat=vector<ll>(2*n,INF); }
+    void set(int i, ll x) {
+        dat[i+=n]=x;
+        while(i>>=1) dat[i]=min(dat[i<<1],dat[i<<1|1]);
+    }
+    ll min(int l, int r) {
+        l+=n; r+=n;
+        ll ret=INF;
+        while(l<r) {
+            if(l&1) chmin(ret,dat[l++]);
+            if(r&1) chmin(ret,dat[--r]);
+            l>>=1; r>>=1;
+        }
+        return ret;
+    }
+};
