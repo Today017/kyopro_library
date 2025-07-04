0fa9b5c5306ab61325356a8f2a69d222e1f9f11c
Today017 <yesterday998244353@gmail.com>
2025-07-04 11:13:14 +0900

    LCA修正

--- graph/tree/lca.hpp
+++ graph/tree/lca.hpp
@@ -3,17 +3,14 @@
 /// @brief LCA
 /// @ref verify: https://onlinejudge.u-aizu.ac.jp/status/users/Today03/submissions/1/GRL_5_C/judge/10572843/C++17
 struct LCA {
-    /// @brief コンストラクタ
     LCA(const vector<vector<int>>& g, int root=0) {
-        int n=g.size();
-        int k=1;
+        int n=g.size(), k=1;
         while((1<<k)<n) k++;
         par=vector<vector<int>>(k,vector<int>(n,-1)),dep=vector<int>(n);
         dfs(g,root,-1);
         for(int i=0; i<k-1; i++) for(int j=0; j<n; j++) par[i+1][j]=(par[i][j]==-1?-1:par[i][par[i][j]]);
     }
-    
-    /// @brief 頂点 u, v のLCAを返す
+
     int lca(int u, int v) {
         if(dep[u]<dep[v]) swap(u,v);
         int k=par.size();
@@ -23,17 +20,10 @@ struct LCA {
         return par[0][u];
     }
 
-    /// @brief 頂点 u, v の距離を返す
-    int distance(int u, int v) {
-        return dep[u]+dep[v]-2*dep[lca(u,v)];
-    }
+    int distance(int u, int v) { return dep[u]+dep[v]-2*dep[lca(u,v)]; }
 
-    /// @brief 頂点 x が u-v のパス上にあるか否かを返す
-    bool is_on_path(int u, int v, int x) {
-        return distance(u,x)+distance(x,v)==distance(u,v);
-    }
+    bool is_on_path(int u, int v, int x) { return distance(u,x)+distance(x,v)==distance(u,v); }
 
-    /// @brief 頂点 u から d 回遡った位置にある頂点を返す
     int climb(int u, int d) {
         int k=par.size();
         for(int i=k-1; i>=0; i--) if(d>>i&1) u=par[i][u];
@@ -42,6 +32,7 @@ struct LCA {
 
     vector<vector<int>> par;
     vector<int> dep;
+
 private:
     void dfs(const vector<vector<int>>& g, int now, int pre) {
         par[0][now]=pre; dep[now]=(pre==-1 ? 0 : dep[pre]+1);
