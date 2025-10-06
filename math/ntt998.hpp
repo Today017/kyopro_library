9884c2c8953d7be8e5f0f90a8e6a05a3f315314b
Today017 <yesterday998244353@gmail.com>
2025-10-06 02:01:09 +0000

    NTT高速化

--- math/ntt998.hpp
+++ math/ntt998.hpp
@@ -5,33 +5,40 @@
 /// @note O(N log(N))
 /// @details f(x) = Σ a[i]x^i, w^N = 1 とすると、F(t) = Σ f(w^i)t^i の各係数 mod 998244353 に変換する
 void NTT998(vector<Mod998>& a, bool inv=false) {
-    int n=a.size(),h=0;
-    while((1<<h)<n) h++;
-    for(int i=0; i<n; i++) {
-        int j=0;
-        // ビットを逆に
-        for(int k=0; k<h; k++) j|=(i>>k&1)<<(h-1-k);
-        if(i<j) swap(a[i],a[j]);
-    }
-
-    const vector<Mod998> rt={1,998244352,911660635,372528824,929031873,452798380,922799308,781712469,476477967,166035806,258648936,584193783,63912897,350007156,666702199,968855178,629671588,24514907,996173970,363395222,565042129,733596141,267099868,15311432};
-    const vector<Mod998> irt={1,998244352,86583718,509520358,337190230,87557064,609441965,135236158,304459705,685443576,381598368,335559352,129292727,358024708,814576206,708402881,283043518,3707709,121392023,704923114,950391366,428961804,382752275,469870224};
-
-    for(int b=1,t=1; b<n; b<<=1,t++) {
-        Mod998 w=1,base=inv?irt[t]:rt[t];
-        for(int j=0; j<b; j++) {
-            // w := 1 の b 乗根の j 乗
-            for(int k=0; k<n; k+=b*2) {
-                Mod998 s=a[j+k], t=a[j+k+b]*w;
-                a[j+k]=s+t; a[j+k+b]=s-t;
-            }
-            w*=base;
+    static bool flag=false;
+    static int divide_max;
+    static vector<Mod998> roots, inv_roots, tmp;
+    if(!flag) {
+        flag=true;
+        divide_max=1;
+        ll n=998244353-1;
+        while(n%2==0) n>>=1,divide_max++;
+        roots=vector<Mod998>(divide_max+1);
+        inv_roots=vector<Mod998>(divide_max+1);
+        roots[0]=inv_roots[0]=1;
+        for(int i=1; i<=divide_max; i++) {
+            roots[i]=Mod998(3).pow((998244353-1)/(1<<i));
+            inv_roots[i]=roots[i].inv();
         }
     }
 
+    tmp=vector<Mod998>(a.size());
+    int n=a.size(), mask=n-1, p=0;
+    for(int i=n>>1; i>=1; i>>=1) {
+        auto& cur=((p&1) ? tmp : a);
+        auto& nxt=((p&1) ? a : tmp);
+        Mod998 e=(inv ? roots[p+1] : inv_roots[p+1]);
+        Mod998 w=1;
+        for(int j=0; j<n; j+=i) {
+            REP(k,i) nxt[j+k]=cur[((j<<1)&mask)+k]+w*cur[(((j<<1)+i)&mask)+k];
+            w*=e;
+        }
+        p++;
+    }
+    if(p&1) swap(a,tmp);
     if(inv) {
-        Mod998 inv_n=Mod998(n).inv();
-        for(int i=0; i<n; i++) a[i]*=inv_n;
+        Mod998 inv_sz=Mod998(n).inv();
+        for(int i=0; i<n; i++) a[i]*=inv_sz;
     }
 }
 
