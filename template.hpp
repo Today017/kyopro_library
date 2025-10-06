553a5178c2fe039e4802001ce4737bc6929c29b1
Today017 <yesterday998244353@gmail.com>
2025-10-06 02:00:57 +0000

    テンプレ更新

--- template.hpp
+++ template.hpp
@@ -2,18 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ALL(x) (x).begin(), (x).end()
-#define REP(i, n) for(ll i=0; i<(ll)(n); i++)
-
-template<typename T> bool chmax(T& a, T b) { return a<b ? a=b, true : false; }
-template<typename T> bool chmin(T& a, T b) { return a>b ? a=b, true : false; }
+#define REP(i,n) for(int i=0; i<n; i++)
+bool chmax(auto& a, auto b) { return a<b ? a=b, true : false; }
+bool chmin(auto& a, auto b) { return a>b ? a=b, true : false; }
 using ll=long long; const int INF=1e9+10; const ll INFL=4e18;
-using VI=vector<int>; using VVI=vector<VI>; using VL=vector<ll>; using VVL=vector<VL>;
-using PL=pair<ll,ll>; using VP=vector<PL>; using WG=vector<vector<pair<int,ll>>>;
 
-#ifdef LOCAL
+#ifdef DEBUG
 #include "./debug.hpp"
 #else
 #define debug(...)
 #define print_line
 #endif
-
