abb7d9ae38243f0f127ca321a6a6ecda80661581
Today017 <yesterday998244353@gmail.com>
2026-05-11 18:08:48 +0900

    template修正

--- template.hpp
+++ template.hpp
@@ -6,6 +6,8 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 bool chmax(auto& a, auto b) { return a<b ? a=b, 1: 0; }
 bool chmin(auto& a, auto b) { return a>b ? a=b, 1: 0; }
+int lob(auto& v, auto x) { return lower_bound(all(v),x)-v.begin(); }
+void uniq(auto& v) { sort(all(v)), v.erase(unique(all(v)),v.end()); }
 using ll=long long; const int INF=1e9+10; const ll INFL=4e18;
 
 #ifdef DEBUG
